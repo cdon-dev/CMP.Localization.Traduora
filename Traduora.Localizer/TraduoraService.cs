a024d3d7eab07e16f068c2fddf42a4f726619b0a
Vullnet Dyla <vullnet.dyla@gmail.com>
2019-04-05 17:02:06 +0200

    Implemented exception handling for traduora call failure

--- Traduora.Localizer/TraduoraService.cs
+++ Traduora.Localizer/TraduoraService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Traduora.Localizer.Config;
@@ -19,9 +21,21 @@ namespace Traduora.Localizer
 
         public async Task<IReadOnlyDictionary<string, IReadOnlyDictionary<string, string>>> GetTranslations()
         {
-            string key = await _traduora.Authenticate(_config.ClientId, _config.ClientSecret);
+            try
+            {
+                string key = await _traduora.Authenticate(_config.ClientId, _config.ClientSecret);
 
-            return await _traduora.GetData(_config.ProjectId, key);
+                return await _traduora.GetData(_config.ProjectId, key);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                var emptyDictionary = new ReadOnlyDictionary<string, IReadOnlyDictionary<string, string>>
+                    (new Dictionary<string, IReadOnlyDictionary<string, string>>());
+
+                return emptyDictionary;
+            }
         }
     }
 }
